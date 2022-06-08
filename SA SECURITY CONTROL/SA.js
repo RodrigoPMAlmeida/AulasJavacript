fa57f5e20fea5c14e030962554e3172a14488b14
RodrigoPMAlmeida <rodrigo.monteiro.almeida@hotmail.com>
2022-06-07 21:02:30 -0300

    Add files via upload

--- SA SECURITY CONTROL/SA.js
+++ SA SECURITY CONTROL/SA.js
@@ -3,55 +3,77 @@ let Conta = {}
 
 function Cadastro(){
 
-    cadastroUsuario = JSON.parse(localStorage.getItem(`Produtos`))    
+    cadastroUsuario = JSON.parse(localStorage.getItem(`Logins`))    
    
     if(cadastroUsuario == null){
         
         cadastroUsuario = []
         Conta = {
 
-            email: document.getElementById("email").value,
+            usuario: document.getElementById("usuario").value,
             senha:  document.getElementById("senha").value
 
         }
-        if(Conta.email != "" && Conta.senha != "")
+        if(Conta.usuario != "" && Conta.senha != "")
 {        
             cadastroUsuario.push(Conta)
-            localStorage.setItem(`Produtos`, JSON.stringify(cadastroUsuario))
+            localStorage.setItem(`Logins`, JSON.stringify(cadastroUsuario))
             alert("e-mail e senha cadastrados!!")
 }  
-
         else{
-        alert("Insira todos os dados!");
+            alert("Insira todos os dados!");
         }
-
 }
     else{
-
         Conta = {
-
-            email: document.getElementById("email").value,
+            usuario: document.getElementById("usuario").value,
             senha:  document.getElementById("senha").value
-    }
+        }
 
-        if(Conta.email != "" && Conta.senha != ""){
+        
+        if(Conta.usuario != "" && Conta.senha != ""){
                
                     cadastroUsuario.push(Conta)
-                    localStorage.setItem(`Produtos`, JSON.stringify(cadastroUsuario))
-                    alert("e-mail e senha cadastrados!!")
-            }  
+                    localStorage.setItem(`Logins`, JSON.stringify(cadastroUsuario))
+                    alert("Usuário e senha cadastrados!!")
+        }  
         
         else{
-            
-                alert("Insira todos os dados!");
+            alert("Insira todos os dados!");
 
         }
-}
+    }
 }
 
 function Login(){
 
-    cadastroUsuario = JSON.parse(localStorage.getItem(`Produtos`))
+    cadastroUsuario = JSON.parse(localStorage.getItem(`Logins`));
+    usuarioDigitado = document.getElementById("usuario").value;
+    senhaDigitada = document.getElementById("senha").value;
+    validacao = false;
 
+    if(cadastroUsuario == null){
+            alert("Usuário ou senha não encontrado!")
+        }
+    
+    else{
+        for(var i = 0; cadastroUsuario[i]; i++){
+            if(usuarioDigitado == cadastroUsuario[i].usuario && senhaDigitada == cadastroUsuario[i].senha){
+                alert(`Bem-vindo ${cadastroUsuario[i].usuario}`);
+                validacao = true
+         }
+     }
+        if(validacao == true){
+            alert("Usuário logado com sucesso!");
+            IrParaIndex();
+        }
+        else{
+            alert("Usuário ou senha não encontrado!")
+        }
+    
+    }
+}
 
+function IrParaIndex(){
+    window.location.href = "index.html";
 }
