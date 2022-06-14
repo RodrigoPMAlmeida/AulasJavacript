92da4e82fc1a5070a74180576c3eb4b3010e2a79
guilhermesimas21 <103226863+guilhermesimas21@users.noreply.github.com>
2022-06-14 19:32:38 -0300

    Add files via upload

--- SA SECURITY CONTROL/TelaDeCadastroSeguranca.js
+++ SA SECURITY CONTROL/TelaDeCadastroSeguranca.js
@@ -10,14 +10,36 @@ function MostrarUsuarioAtual(){
 }
 
 
-function cadastrarSegurança(){
+function cadastrarSeguranca(){
      
     listaSeguranca = JSON.parse(localStorage.getItem('Segurancas'))
      
     if(listaSeguranca == null){
+
         listaSeguranca = []
+        Seguranca = {
 
+            nome: document.getElementById("nomeSegurança1").value,
+            cpf: document.getElementById("cpfSegurança1").value,
+            telefone: document.getElementById("telefoneSegurança1").value,
+            idade: document.getElementById("idadeSegurança1").value,
 
+        }
+
+    if(Seguranca.nome != "" && Seguranca.cpf != "" && Seguranca.telefone!= "" && Seguranca.idade != "" )
+                   
+            listaSeguranca.push(Seguranca)
+            localStorage.setItem(`SegurancasADM`, JSON.stringify(listaSeguranca))
+
+        
     }
+
+}
+
+function Deslogar(){
+    usuarioAtual = JSON.parse(localStorage.getItem('UsuarioAtual'))
+    usuarioAtual = null;
+    localStorage.setItem(`UsuarioAtual`, JSON.stringify(usuarioAtual))
+    alert("Deslogando...")
 
 }
